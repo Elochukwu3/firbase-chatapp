9238105ffc27b351172bbf362eba2d3f49a8d571
Elochukwu3 <oguajuromauld5@gmail.com>
2022-12-28 19:53:34 +0100

    sass installed

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
+import SignUp from "./pages/SignUp";
+import './style.scss';
+
 function App() {
   return (
-    <div className="chat-bar-collapsible">
-      {" "}
-      <button id="chat-button" type="button" className="collapsible">
-        Chat with us!
-        {/* <i
-          id="chat-icon"
-          style="color: #fff;"
-          class="fa fa-fw fa-comments-o"
-        ></i> */}
-      </button>
-      <div className="content">
-        <div className="full-chat-block">
-          <div className="outer-container">
-            <div className="chat-container">
-              <div id="chatbox">
-                <h5 id="chat-timestamp"></h5>
-                <p id="botStarterMessage" className="botText">
-                  <span>Loading...</span>
-                </p>
-              </div>
-
-              <div className="chat-bar-input-block">
-                <div id="userInput">
-                  <input
-                    id="textInput"
-                    className="input-box"
-                    type="text"
-                    name="msg"
-                    placeholder="Tap 'Enter' to send a message"
-                  />
-                  <p></p>
-                </div>
-
-                <div className="chat-bar-icons"></div>
-              </div>
-
-              <div id="chat-bar-bottom">
-                <p></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+   <div>
+    <SignUp/>
+   </div>
   );
 }
 
